53cac0430c629f385ca6cf31b905ff7008c063e3
RahulKoli10 <rahulkohli7078@gmail.com>
2026-05-04 16:03:56 +0530

    products NEXT_PUBLIC_BASE_URL change

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -10,7 +10,7 @@ type ProductPageProps = {
 export default async function ProductPage({ params }: ProductPageProps) {
   const { slug } = await params;
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ;
   const res = await fetch(`${baseUrl}/api/product/slug/${encodeURIComponent(slug)}`, {
     cache: "no-store",
   });
